d05058270ad631bc3ef5036590d423037394c8bd
Khubaib Qaiser <khubaib.dev@gmail.com>
2024-09-19 16:25:39 -0400

    Fix element in viewport observer

--- template/amplitude.js
+++ template/amplitude.js
@@ -3,35 +3,51 @@ const getData = (element, attr) => {
   return data ? JSON.parse(data) : {};
 };
 
+// Define the callback function to be executed when the element is in the viewport
+const elementWithinViewport = (element) => {
+  console.log('Element is now in the viewport!', element);
+  window.analytics.logEvent('Slide Rendered', getData(element, 'item'));
+};
+
+// Create a function to handle the intersection logic
+const handleIntersection = (callback) => {
+  return (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        callback(entry.target);
+        observer.unobserve(entry.target); // Stop observing if you only need to detect once
+      }
+    });
+  };
+};
+
+// Create an Intersection Observer instance with the custom callback
+const observer = new IntersectionObserver(handleIntersection(elementWithinViewport), {
+  root: null, // Use the viewport as the root
+  rootMargin: '0px',
+  threshold: 1, // Trigger when 10% of the element is visible
+});
+
 document.addEventListener('DOMContentLoaded', () => {
   window.analytics.logEvent('Page Loaded');
 
   const slides = document.querySelectorAll('.product-section');
 
-  slides.forEach((slide, index) => {
-    slides.onload = () => {
-      // Track slide rendered
-      window.analytics.logEvent('Slide Rendered', { slideIndex: index, ...getData(slide, 'item') });
-    };
+  slides.forEach((slide) => {
+    observer.observe(slide);
     // Track user impressions
     slide.addEventListener('mouseenter', () => {
-      window.analytics.logEvent('Mouse over slide', { slideIndex: index, ...getData(slide, 'item') });
+      window.analytics.logEvent('Mouse over slide', getData(slide, 'item'));
     });
 
     // Track user mouseenter and mouseclick events on elements inside each slide
     slide.querySelectorAll('.suggestion-item').forEach((element) => {
       element.addEventListener('mouseenter', () => {
-        window.analytics.logEvent('Suggestion Hover', {
-          slideIndex: index,
-          ...getData(element, 'item'),
-        });
+        window.analytics.logEvent('Suggestion Hover', getData(element, 'item'));
       });
 
       element.addEventListener('click', () => {
-        window.analytics.logEvent('Suggestion Clicked', {
-          slideIndex: index,
-          ...getData(element, 'item'),
-        });
+        window.analytics.logEvent('Suggestion Clicked', getData(element, 'item'));
       });
     });
   });
