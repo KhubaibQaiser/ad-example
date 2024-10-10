f2981ddf5d6980c938b40933decd2e02fbce4422
Khubaib <khubaib.dev@gmail.com>
2024-10-10 10:18:23 -0400

    Fix issues relatd to namespaces

--- src/generator/templates/carousel-template/amplitude-tracking.js
+++ src/generator/templates/carousel-template/amplitude-tracking.js
@@ -9,7 +9,7 @@
 
   // Define the callback function to be executed when the element is in the viewport
   const elementWithinViewport = (element) => {
-    window.ShopsenseEmbeds.analytics.logEvent('Tile: Rendered', getData(element, 'item'));
+    window.ShopsenseEmbeds.analytics.logEvent('Tile: Rendered', window.ShopsenseEmbeds.getData(element, 'item'));
   };
 
   document.addEventListener('DOMContentLoaded', () => {
@@ -21,17 +21,17 @@
       window.ShopsenseEmbeds.getObserverInstance(slide, elementWithinViewport);
       // Track user impressions
       slide.addEventListener('mouseenter', () => {
-        window.ShopsenseEmbeds.analytics.logEvent('Tile: Mouse Over', getData(slide, 'item'));
+        window.ShopsenseEmbeds.analytics.logEvent('Tile: Mouse Over', window.ShopsenseEmbeds.getData(slide, 'item'));
       });
 
       // Track user mouseenter and mouseclick events on elements inside each slide
       slide.querySelectorAll('.suggestion-item').forEach((element) => {
         element.addEventListener('mouseenter', () => {
-          window.ShopsenseEmbeds.analytics.logEvent('Product: Mouse Hover', getData(element, 'item'));
+          window.ShopsenseEmbeds.analytics.logEvent('Product: Mouse Hover', window.ShopsenseEmbeds.getData(element, 'item'));
         });
 
         element.addEventListener('click', () => {
-          window.ShopsenseEmbeds.analytics.logEvent('Product: Clicked', getData(element, 'item'));
+          window.ShopsenseEmbeds.analytics.logEvent('Product: Clicked', window.ShopsenseEmbeds.getData(element, 'item'));
         });
       });
     });
--- src/generator/templates/curated-products-template/amplitude-tracking.js
+++ src/generator/templates/curated-products-template/amplitude-tracking.js
@@ -10,7 +10,7 @@
 
   // Define the callback function to be executed when the element is in the viewport
   const elementWithinViewport = (element) => {
-    window.ShopsenseEmbeds.analytics.logEvent('Curated: Rendered', getData(element, 'item'));
+    window.ShopsenseEmbeds.analytics.logEvent('Curated: Rendered', window.ShopsenseEmbeds.getData(element, 'item'));
   };
 
   document.addEventListener('DOMContentLoaded', () => {
@@ -25,17 +25,17 @@
     const products = document.querySelectorAll('.product');
     products.forEach((element) => {
       element.addEventListener('mouseenter', () => {
-        window.ShopsenseEmbeds.analytics.logEvent('Product: Mouse Hover', getData(element, 'item'));
+        window.ShopsenseEmbeds.analytics.logEvent('Product: Mouse Hover', window.ShopsenseEmbeds.getData(element, 'item'));
       });
 
       element.addEventListener('click', () => {
-        window.ShopsenseEmbeds.analytics.logEvent('Product: Clicked', getData(element, 'item'));
+        window.ShopsenseEmbeds.analytics.logEvent('Product: Clicked', window.ShopsenseEmbeds.getData(element, 'item'));
       });
     });
 
     var shopNowCtaButton = document.querySelector('.collection-cta-button');
     shopNowCtaButton.addEventListener('click', () => {
-      window.ShopsenseEmbeds.analytics.logEvent('CTA: Shop Now Clicked', getData(shopNowCtaButton, 'item'));
+      window.ShopsenseEmbeds.analytics.logEvent('CTA: Shop Now Clicked', window.ShopsenseEmbeds.getData(shopNowCtaButton, 'item'));
     });
   });
 })();
