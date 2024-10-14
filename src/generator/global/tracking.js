c285dd4ae2509fd78c758b05deda21f5d7da8fd1
Khubaib <khubaib.dev@gmail.com>
2024-10-14 02:36:50 -0400

    Fix amplitude wrapper

--- src/generator/global/tracking.js
+++ src/generator/global/tracking.js
@@ -2,23 +2,10 @@
   // Ensure the namespace exists
   window.ShopsenseEmbeds = window.ShopsenseEmbeds || {};
 
-  const getUtmParams = () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    return {
-      utm_source: urlParams.get('utm_source'),
-      utm_medium: urlParams.get('utm_medium'),
-      utm_campaign: urlParams.get('utm_campaign'),
-      utm_term: urlParams.get('utm_term'),
-      utm_content: urlParams.get('utm_content'),
-    };
-  };
-
-  window.ShopsenseEmbeds.getUtmParams = getUtmParams;
-
   const iframe = document.querySelector('#adIframe');
 
   if (iframe) {
-    const utmParams = getUtmParams();
+    const utmParams = window.ShopsenseEmbeds.getUtmParams();
     // Send UTM parameters to the iframe
     iframe.onload = () => {
       iframe.contentWindow.postMessage(utmParams, '*');
