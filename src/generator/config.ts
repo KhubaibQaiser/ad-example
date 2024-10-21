f2ece77895b13bacb27ee398ea1068803c0cb05d
Khubaib <khubaib.dev@gmail.com>
2024-10-21 10:15:36 -0400

    Remove unwanted deps

--- src/generator/config.ts
+++ src/generator/config.ts
@@ -1,56 +1,7 @@
 import path from 'path';
 import dotenv from 'dotenv';
-// import yargs from 'yargs/yargs';
-// import { hideBin } from 'yargs/helpers';
 
 const isProduction = process.env.NODE_ENV === 'production';
-
-// Parse command-line arguments
-// export const argv = yargs(hideBin(process.argv))
-//   .options({
-//     width: {
-//       alias: 'w',
-//       type: 'number',
-//       description: 'Width for the ad unit',
-//       default: 160,
-//     },
-//     height: {
-//       alias: 'h',
-//       type: 'number',
-//       description: 'Height for the ad unit',
-//       default: 600,
-//     },
-//     quality: {
-//       alias: 'q',
-//       type: 'number',
-//       description: 'Quality for image compression',
-//       default: 80,
-//     },
-//     'compress-video': {
-//       alias: 'cv',
-//       type: 'boolean',
-//       description: 'Enable or disable video compression',
-//       default: false,
-//     },
-//     'store-handle': {
-//       alias: 'sh',
-//       type: 'string',
-//       description: 'Comma separated list of store handles',
-//       demandOption: true,
-//     },
-//     'collection-handle': {
-//       alias: 'ch',
-//       type: 'string',
-//       description: 'Comma separated list of collection handles',
-//       demandOption: true,
-//     },
-//     template: {
-//       alias: 't',
-//       type: 'string',
-//       description: 'Comma separated list of templates to use. If no option is provided ad will be generated for all available templates.',
-//     },
-//   })
-//   .parseSync();
 
 const envFile = `.env.${isProduction ? 'production' : 'development'}`;
 dotenv.config({ path: envFile });
